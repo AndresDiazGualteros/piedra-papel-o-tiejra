a1413998e6074f5de6dcaba1cfff574a2e420844
luis <andresgualteros2822@gmail.com>
2022-09-20 11:08:57 -0500

    añadir funciones

--- papel.js
+++ papel.js
@@ -1,15 +1,39 @@
-let jugar;
-let opciones;
-jugar = +prompt("¿Cuantas quiere jugar?");
+// let jugar;
+// let opciones;
+// jugar = +prompt("¿Cuantas quiere jugar?");
 
-for (let i = 0; i < jugar; i++) {
-  let pc = Math.floor(Math.random() * 3 + 1);
-  opciones = prompt("elige el numero: 1.pidra, 2.papel, 3.tijeras");
-  if (opciones == pc) {
+// for (let i = 0; i < jugar; i++) {
+//   let pc = Math.floor(Math.random() * 3 + 1);
+//   opciones = prompt("elige el numero: 1.pidra, 2.papel, 3.tijeras");
+//   if (opciones == pc) {
+//     alert("empataron");
+//   } else if (opciones > pc) {
+//     alert("el usuario gano");
+//   } else if (opciones < pc) {
+//     alert("el pc gano");
+//   }
+// }
+let opcion, veces, jugador, pc;
+
+function eleccionpc() {
+  pc = Math.floor(Math.random() * 3 + 1);
+}
+
+let cantidad = function () {
+  let juego = +prompt("ingrese cuantas veces quiere jugar");
+  return juego;
+};
+
+jugador = cantidad();
+
+for (let i = 0; i < jugador; i++) {
+  eleccionpc();
+  opcion = +prompt("elije una obcion: 1.piedra, 2.papel o 3.tijera");
+  if (opcion == pc) {
     alert("empataron");
-  } else if (opciones > pc) {
+  } else if (opcion > pc) {
     alert("el usuario gano");
-  } else if (opciones < pc) {
+  } else if (opcion < pc) {
     alert("el pc gano");
   }
 }
